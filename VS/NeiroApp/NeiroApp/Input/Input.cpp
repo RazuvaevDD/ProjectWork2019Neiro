0c5d0ee563e3e5c18085bbb63e1344a7c90d0116
RazuvaevDD <Razuvaev_DD@mail.ru>
2019-03-16 19:11:26 +0000

    Added Input module base.

---
+++ VS/NeiroApp/NeiroApp/Input/Input.cpp
@@ -0,0 +1,13 @@
+#include "Input\Input.hpp"
+#include <iostream>
+
+using namespace Input_module;
+
+Input::Input()
+{
+}
+
+
+Input::~Input()
+{
+}
--- VS/NeiroApp/NeiroApp/main.cpp
+++ VS/NeiroApp/NeiroApp/main.cpp
@@ -1,6 +1,7 @@
 #include "Output/Mouse.hpp"
 #include "Output/Keyboard.hpp"
 #include "GUI/GUI.hpp"
+//#include "Input/Input.hpp"
 
 int main(int argc, char *argv[])
 {
@@ -8,6 +9,8 @@ int main(int argc, char *argv[])
 
 	Output_module::Mouse mouse;
 	Output_module::Keyboard keyboard;
+
+	//Input_module::Input input;
 	
 	mouse.shiftCoords(100, 50, 1000);
 	mouse.setCoords(0, 0, 1000);
