1a1d3f50a5f75a70c13d8a62d1670cac87ce450a
AmRust <rustam3799@gmail.com>
2019-07-22 19:33:38 +0300

    Fixed key bug

--- VS/NeuroApp/NeuroApp/Output/Keyboard.cpp
+++ VS/NeuroApp/NeuroApp/Output/Keyboard.cpp
@@ -14,9 +14,21 @@ void Keyboard::pressButtonSlt(std::string sKey)
 	INPUT input;
 	WORD vKey = getKeyCode(sKey);
 	input.type = INPUT_KEYBOARD;
-	input.ki.wScan = MapVirtualKey(vKey, MAPVK_VK_TO_VSC);
+	input.ki.wScan = 0;
+	input.ki.time = 0;
+	input.ki.dwExtraInfo = 0;
 	input.ki.wVk = vKey;
+	input.ki.dwFlags = 0;
 	SendInput(1, &input, sizeof(INPUT));
+
+	std::string command = "echo TRY TO PRESS: " + sKey + " >> keyboard.log";
+	//system(command.c_str());
+	//keybd_event(vKey, 0x45, KEYEVENTF_EXTENDEDKEY | 0, 0);
+	if (GetAsyncKeyState(vKey))
+	{
+		command = "echo SUCCESFULLY PRESSED: " + sKey + " >> keyboard.log";
+		//system(command.c_str());
+	}
 }
 
 
@@ -24,11 +36,25 @@ void Keyboard::releaseButtonSlt(std::string sKey)
 {
 	INPUT input;
 	WORD vKey = getKeyCode(sKey);
+
 	input.type = INPUT_KEYBOARD;
-	input.ki.wScan = MapVirtualKey(vKey, MAPVK_VK_TO_VSC);
+	input.ki.wScan = 0;
+	input.ki.time = 0;
+	input.ki.dwExtraInfo = 0;
 	input.ki.wVk = vKey;
 	input.ki.dwFlags = KEYEVENTF_KEYUP;
 	SendInput(1, &input, sizeof(INPUT));
+
+	std::string command = "echo TRY TO RELEASE: " + sKey + " >> keyboard.log";
+	//system(command.c_str());
+	//keybd_event(vKey, 0x45, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, 0);
+	if (!GetAsyncKeyState(vKey))
+	{
+		command = "echo SUCCESFULLY RELEASED: " + sKey + " >> keyboard.log";
+		//system(command.c_str());
+	}
+	command = "echo ================================== >> keyboard.log";
+	//system(command.c_str());
 }
 
 byte Keyboard::getKeyCode(std::string sKey)
